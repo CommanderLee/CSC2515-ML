0d13d94b4c36fcd7501b8f78468159c35135b439
Zhen Li <zhen@cs.toronto.edu>
2015-10-18 23:00:08 -0400

    Complete 2.2

--- A1/logistic_regression_template.m
+++ A1/logistic_regression_template.m
@@ -9,7 +9,7 @@ load mnist_valid;
 
 %% TODO: Initialize hyperparameters.
 % Learning rate
-hyperparameters.learning_rate = 1;
+hyperparameters.learning_rate = 0.5;
 % Weight regularization parameter
 hyperparameters.weight_regularization = 1;
 % Number of iterations
@@ -33,33 +33,47 @@ diff = checkgrad('logistic', ...
 
 %% Begin learning with gradient descent.
 % N = size(mnist_train, 0);
-learn_rate_set = [0.5 0.75 1];
+learn_rate_set = [0.25 0.5 1];
 num_iter_set = [100 300 500];
-results = zeros(9, 6);
+hyperNum = 9;
+results = zeros(hyperNum, 6);
 
-for hyperID = 1:9
-    hyperparameters.learning_rate = learn_rate_set(fix((hyperID-1)/3)+1);
-    hyperparameters.num_iterations = num_iter_set(mod(hyperID, 3)+1);
+testing = 1;
+if testing
+   hyperNum = 1; 
+end
+
+for hyperID = 1:hyperNum
+    if ~testing
+        hyperparameters.learning_rate = learn_rate_set(fix((hyperID-1)/3)+1);
+        hyperparameters.num_iterations = num_iter_set(mod(hyperID, 3)+1);
+    end
     
-    ce_t_sum = 0.0;
-    error_t_sum = 0.0;
-    ce_v_sum = 0.0;
-    error_v_sum = 0.0;
+    ce_train = [];
+    er_train = [];
+    ce_valid = [];
+    er_valid = [];
+    ce_test = [];
+    er_test = [];
     
-    multi_times = 20;
+    multi_times = 5;
+    colors = ['r', 'b', 'k', 'g', 'c'];
     for mt = 1:multi_times
+        weights = randn(M+1, 1) * 0.1;
+        
+        y_train = zeros(1, hyperparameters.num_iterations);
+        y_valid = zeros(1, hyperparameters.num_iterations);
         for t = 1:hyperparameters.num_iterations
-
-            %% TODO: You will need to modify this loop to create plots etc.
 
             % Find the negative log likelihood and derivative w.r.t. weights.
             [f, df, predictions] = logistic(weights, ...
                                             train_inputs, ...
-                                            train_targets, ...
+                                            train_targets, ...                                     
                                             hyperparameters);
 
             [cross_entropy_train, frac_correct_train] = evaluate(train_targets, predictions);
-
+%             [cross_entropy_train, frac_correct_train] = evaluate(train_targets_small, predictions);
+            
             % Find the fraction of correctly classified validation examples.
             [temp, temp2, frac_correct_valid] = logistic(weights, ...
                                                          valid_inputs, ...
@@ -77,17 +91,63 @@ for hyperID = 1:9
             [cross_entropy_valid, frac_correct_valid] = evaluate(valid_targets, predictions_valid);
 
             %% Print some stats.
-%             fprintf(1, 'ITERATION:%4i   NLOGL:%4.2f TRAIN CE %.6f TRAIN FRAC:%2.2f VALIC_CE %.6f VALID FRAC:%2.2f\n',...
-%                     t, f/N, cross_entropy_train, frac_correct_train*100, cross_entropy_valid, frac_correct_valid*100);
+%              fprintf(1, 'ITERATION:%4i   NLOGL:%4.2f TRAIN CE %.6f TRAIN FRAC:%2.2f VALIC_CE %.6f VALID FRAC:%2.2f\n',...
+%                      t, f/N, cross_entropy_train, frac_correct_train*100, cross_entropy_valid, frac_correct_valid*100);
+            y_train(t) = cross_entropy_train;
+            y_valid(t) = cross_entropy_valid;
         end
-        ce_t_sum = ce_t_sum + cross_entropy_train;
-        error_t_sum = error_t_sum + (1 - frac_correct_train);
-        ce_v_sum = ce_v_sum + cross_entropy_valid;
-        error_v_sum = error_v_sum + (1 - frac_correct_valid);
+        ce_train = [ce_train, cross_entropy_train];
+        er_train = [er_train, (1 - frac_correct_train)];
+        ce_valid = [ce_valid, cross_entropy_valid];
+        er_valid = [er_valid, (1 - frac_correct_valid)];
+        
+        if testing
+            load mnist_test;
+            predictions_test = logistic_predict(weights, test_inputs);
+            [cross_entropy_test, frac_correct_test] = evaluate(test_targets, predictions_test);
+            ce_test = [ce_test cross_entropy_test];
+            er_test = [er_test 1 - frac_correct_test];
+        end
+        
+        %% Plot train & validation
+%         figure(mt);
+
+        x = 1:hyperparameters.num_iterations;
+        plot(x, y_train, strcat(colors(mt),'-'), x, y_valid, strcat(colors(mt), ':'));
+        xlabel('iteration');
+        ylabel('cross entropy');
+        legend('train','validation', -1);
+%         title('mnist\_train\_small');
+        title('mnist\_train');
+        hold on;
     end
     results(hyperID, :) = [hyperparameters.learning_rate, hyperparameters.num_iterations, ...
-        ce_t_sum / multi_times, error_t_sum / multi_times, ce_v_sum / multi_times, error_v_sum / multi_times];
+        mean(ce_train), mean(er_train), mean(ce_valid), mean(er_valid)];
 end
 disp(results);
 
-
+%% Plot test
+if testing
+    disp(ce_test');
+    disp(er_test');
+    
+%     figure(multi_times+1);
+%     x = 1:multi_times;
+%     y1 = ce_test;
+%     y2 = er_test;
+%     [AX,H1,H2] = plotyy(x, y1, x, y2, 'plot');
+%     set(AX(1), 'XColor','k','YColor','b');
+%     set(AX(2), 'XColor','k','YColor','r');
+%     HH1 = get(AX(1), 'Ylabel');
+%     set(HH1, 'String', 'test\_cross\_entropy');
+%     set(HH1, 'color', 'b');
+%     HH2=get(AX(2),'Ylabel');
+%     set(HH2, 'String', 'test\_classification\_error');
+%     set(HH2, 'color', 'r');
+%     set(H1, 'LineStyle', '-');
+%     set(H1, 'color', 'b');
+%     set(H2, 'LineStyle', ':');
+%     set(H2, 'color', 'r');
+% 	legend([H1,H2], {'test\_cross\_entropy';'test\_classification\_error'});
+%     xlabel('test id(1 to 20)');
+end
